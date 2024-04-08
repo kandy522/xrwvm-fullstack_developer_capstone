f039d683d0acdc92697bdf254f65b0b9e58b8a83
Kandy Yeung <kandy522@gmail.com>
2024-04-08 12:43:37 -0700

    jshint4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,6 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
@@ -50,7 +50,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+#    context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -59,7 +59,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+#    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
